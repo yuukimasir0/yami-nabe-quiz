0e6c21355a8adc230f6d55d8c23f52aa53cf159a
yuukimasir0 <plusion00@icloud.com>
2024-11-02 20:52:10 +0900

    sql 書いた

--- adapter/migrations/20241021001148_start.down.sql
+++ adapter/migrations/20241021001148_start.down.sql
@@ -1,2 +1,12 @@
 -- Add down migration script here
 DROP FUNCTION set_updated_at;
+
+DROP TRIGGER IF EXISTS quiz_questions_updated_at_trigger ON quiz_questions;
+DROP TRIGGER IF EXISTS quiz_answers_updated_at_trigger ON quiz_answers;
+DROP TRIGGER IF EXISTS scored_quiz_updated_at_trigger ON scored_quiz;
+
+DROP TABLE IF EXISTS quiz_questions;
+DROP TABLE IF EXISTS quiz_answers;
+DROP TABLE IF EXISTS cooked_quizzes;
+DROP TABLE IF EXISTS recipe;
+DROP TABLE IF EXISTS scored_quiz;
--- adapter/migrations/20241021001148_start.up.sql
+++ adapter/migrations/20241021001148_start.up.sql
@@ -6,18 +6,51 @@ CREATE OR REPLACE FUNCTION set_updated_at() RETURNS trigger AS '
     END;
 ' LANGUAGE 'plpgsql';
 
-CREATE TABLE IF NOT EXISTS quizzes(
+CREATE TABLE IF NOT EXISTS quiz_questions(
     quiz_id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-    question TEXT NOT NULL
+    question TEXT NOT NULL,
+    created_at TIMESTAMP(3) WITH TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    updated_at TIMESTAMP(3) WITH TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP(3)
 );
 
-CREATE TABLE IF NOT EXISTS answers(
+CREATE TRIGGER quiz_questions_updated_at_trigger
+    BEFORE UPDATE ON quiz_questions FOR EACH ROW
+    EXECUTE PROCEDURE set_updated_at();
+
+
+CREATE TABLE IF NOT EXISTS quiz_answers(
     answer_id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-    quiz_id UUID REFERENCES quizzes(quiz_id),
-    answer TEXT NOT NULL
+    quiz_id UUID REFERENCES quiz_questions(quiz_id),
+    answer TEXT NOT NULL,
+    created_at TIMESTAMP(3) WITH TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    updated_at TIMESTAMP(3) WITH TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP(3)
 );
 
-CREATE TABLE IF NOT EXISTS good_quiz(
-    quiz_id UUID REFERENCES quizzes(quiz_id),
-    score INTEGER
+CREATE TRIGGER quiz_answers_updated_at_trigger
+    BEFORE UPDATE ON quiz_answers FOR EACH ROW
+    EXECUTE PROCEDURE set_updated_at();
+
+CREATE TABLE IF NOT EXISTS cooked_quizzes(
+    cooked_quiz_id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    cooked_questions TEXT NOT NULL,
+    created_at TIMESTAMP(3) WITH TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP(3)
 );
+
+CREATE TABLE IF NOT EXISTS recipe (
+    recipe_id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    cooked_quiz_id UUID REFERENCES cooked_quizzes(cooked_quiz_id),
+    quiz_id UUID REFERENCES quiz_questions(quiz_id),
+    maked_at TIMESTAMP(3) WITH TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP(3)
+);
+
+CREATE TABLE IF NOT EXISTS scored_quiz(
+    score_id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    cooked_quiz_id UUID REFERENCES cooked_quizzes(cooked_quiz_id),
+    score INTEGER,
+    created_at TIMESTAMP(3) WITH TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    updated_at TIMESTAMP(3) WITH TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP(3)
+);
+
+CREATE TRIGGER scored_quiz_updated_at_trigger
+    BEFORE UPDATE ON scored_quiz FOR EACH ROW
+    EXECUTE PROCEDURE set_updated_at();
