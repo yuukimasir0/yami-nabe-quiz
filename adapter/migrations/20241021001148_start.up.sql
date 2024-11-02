1d920054ce652ef56f7ae57a32f9bd13614fe222
yuukimasir0 <plusion00@icloud.com>
2024-11-02 09:51:04 +0900

    sqlの構文相変わらず忘れる

--- adapter/migrations/20241021001148_start.up.sql
+++ adapter/migrations/20241021001148_start.up.sql
@@ -7,17 +7,17 @@ CREATE OR REPLACE FUNCTION set_updated_at() RETURNS trigger AS '
 ' LANGUAGE 'plpgsql';
 
 CREATE TABLE IF NOT EXISTS quizzes(
-    quiz_id: UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-    question: TEXT NOT NULL
+    quiz_id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    question TEXT NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS answers(
-    answer_id: UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-    quiz_id: UUID REFERENCES quizzes(quiz_id),
-    answer: TEXT NOT NULL
+    answer_id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    quiz_id UUID REFERENCES quizzes(quiz_id),
+    answer TEXT NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS good_quiz(
-    quiz_id: UUID REFERENCES quizzes(quiz_id),
-    score: INTEGER
-)
+    quiz_id UUID REFERENCES quizzes(quiz_id),
+    score INTEGER
+);
